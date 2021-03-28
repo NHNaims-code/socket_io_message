a10a8bf4adf0d2888323329e39906dc8568bfbf0
NHNaims-code <mdnimurhasan99@gmail.com>
2021-03-28 15:03:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+io.on('connection', (socket) => {
+  socket.broadcast.emit('hi');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+  });
+
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
